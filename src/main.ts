c5bd6eb4611c00c5f1339fd1a5aa7010b6adf7db
guianderson <guilherme.anderson34@outlook.com>
2025-02-17 21:36:18 -0300

    increasing body parser request limit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import * as bodyParser from 'body-parser';
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -9,6 +12,10 @@ async function bootstrap() {
       whitelist: true,
       transform: true,
     }));
+
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
