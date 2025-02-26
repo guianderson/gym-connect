3a4e2bfeade2d9b5b21497a96e1445d3e6b1c2e7
guianderson <guilherme.anderson34@outlook.com>
2025-02-25 23:08:36 -0300

    created util image2base64

--- src/utils/image2base64.ts
+++ src/utils/image2base64.ts
@@ -1,6 +1,4 @@
-import * as fs from 'fs';
-
-export function encodeImageToBase64(imagePath: string): string {
-    const imageBuffer = fs.readFileSync(imagePath);
-    return imageBuffer.toString('base64');
+export async function image2Base64(file?: Express.Multer.File) {
+  if (!file) return '';
+  return file.buffer.toString('base64');
 }
